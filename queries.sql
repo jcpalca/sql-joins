0ef5d893ced88d7ec47f3c64beeebb598a58872d
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-18 12:33:44 -0700

    Minor fixes

--- queries.sql
+++ queries.sql
@@ -1,5 +1,6 @@
 -- Join the two tables so that every column and record appears,
 -- regardless of if there is not an owner_id.
+
 SELECT *
   FROM owners AS o
     FULL OUTER JOIN vehicles AS v
@@ -8,11 +9,11 @@ SELECT *
 -- Display the owners first_name and count of vehicles.
 -- The first_name should be ordered in ascending order.
 
-SELECT o.first_name, COUNT(v)
+SELECT o.first_name, COUNT(*)
   FROM owners AS o
     INNER JOIN vehicles AS v
       ON o.id = v.owner_id
-  GROUP BY o.first_name
+  GROUP BY o.id
   ORDER BY o.first_name;
 
 -- Display the owners first_name, average price, and count of vehicles.
@@ -26,7 +27,7 @@ SELECT o.first_name,
   FROM owners AS o
     JOIN vehicles AS v
       ON o.id = v.owner_id
-  GROUP BY o.first_name
+  GROUP BY o.id
   HAVING ROUND(AVG(v.price)) > 10000
          AND COUNT(v) > 1
   ORDER BY o.first_name DESC;
