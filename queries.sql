3e7896a16d215e08fefda20d8676bb38e10afe19
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-18 11:55:05 -0700

    Full outer join implementation

--- queries.sql
+++ queries.sql
@@ -1,1 +1,8 @@
--- write your queries here
+-- Join the two tables so that every column and record appears,
+-- regardless of if there is not an owner_id.
+SELECT *
+ FROM owners AS o
+  FULL OUTER JOIN vehicles AS v
+   ON o.id = v.owner_id;
+
+
