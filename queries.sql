44f6bfed05d49d6b442aa1482ec01352ab509b70
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-18 12:21:25 -0700

    Implement HAVING and aggregate functions

--- queries.sql
+++ queries.sql
@@ -14,3 +14,19 @@ SELECT o.first_name, COUNT(v)
       ON o.id = v.owner_id
   GROUP BY o.first_name
   ORDER BY o.first_name;
+
+-- Display the owners first_name, average price, and count of vehicles.
+-- The first_name should be ordered in descending order.
+-- Only display results with more than one vehicle
+-- and an average price greater than 10000.
+
+SELECT o.first_name,
+       ROUND(AVG(v.price)) AS average_price,
+       COUNT(v)
+  FROM owners AS o
+    JOIN vehicles AS v
+      ON o.id = v.owner_id
+  GROUP BY o.first_name
+  HAVING ROUND(AVG(v.price)) > 10000
+         AND COUNT(v) > 1
+  ORDER BY o.first_name DESC;
